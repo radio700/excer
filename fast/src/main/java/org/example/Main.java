98a9505a7e0cf224dc3d8b7861cce9af7bbdb1fb
jawsbar <radio700@hanmail.net>
2023-05-17 22:59:11 +0900

    start

---
+++ fast/src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        try {
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "JAWSBAR", "1234");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT");
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + " " + rs.getString(2) + " "
+                        + rs.getDate(3) + " " + rs.getString(4)
+                        + " #### " + rs.getInt(5));
+            }
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
